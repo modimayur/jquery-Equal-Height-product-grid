33b108c2ca4e1025e8fabf9a44b4467e1939d7ad
modimayur <modimayur123@gmail.com>
2018-03-09 11:41:41 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(window).on('load resize ready', function() {
+ getEqualHeightGrid();
+ setTimeout(function(){
+   getEqualHeightGrid();
+ }, 1000);
+ setTimeout(function(){
+   getEqualHeightGrid();
+ }, 2000);
+ setTimeout(function(){
+   getEqualHeightGrid();
+ }, 3000);
+ setTimeout(function(){
+   getEqualHeightGrid();
+ }, 4000);
+ setTimeout(function(){
+   getEqualHeightGrid();
+ }, 5000);
+});
+function getEqualHeightGrid() {
+ $('.grid--view-items .grid-view-item').responsiveEqualHeightGrid();
+}
